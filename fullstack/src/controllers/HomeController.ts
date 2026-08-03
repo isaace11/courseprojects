4ed24c9f79a1d41fa79f1e0e26c9d56c4532d748
isaace11 <tiacevedoa@eafit.edu.co>
2026-08-03 12:51:13 -0500

    Modelo de adicion de libros, y ver detalles

--- fullstack/src/controllers/HomeController.ts
+++ fullstack/src/controllers/HomeController.ts
@@ -1,4 +1,6 @@
 import type { Request, Response } from 'express';
+import { books } from '../data/books.js';
+import { Book } from '../models/Book.js';
 
 export class HomeController {
   static index(req: Request, res: Response): void {
@@ -30,4 +32,45 @@ export class HomeController {
       viewData
     });
   }
+
+  static mainPoint(req: Request, res: Response): void {
+    const viewData = {
+      title: 'Books',
+      books
+    };
+
+    res.render('home/books', {
+      viewData,
+      books
+    });
+  }
+
+  static show(req: Request, res: Response): void {
+  const idParam = req.params.id;
+
+  if (!idParam || Array.isArray(idParam)) {
+    res.status(400).send('Book id is invalid');
+    return;
+  }
+
+  const id = Number.parseInt(idParam, 10);
+
+  if (Number.isNaN(id)) {
+    res.status(400).send('Book id must be a number');
+    return;
+  }
+
+  const book = Book.findById(books, id);
+
+  const viewData = {
+    title: book.title
+  };
+
+  res.render('home/show', {
+    viewData,
+    book
+  });
+}
+
+  
 }
